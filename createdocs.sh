8917451917bb7223861414d67eb428c24e3458fd
James Gray <randombeeper@gmail.com>
2025-04-28 15:18:53 -0600

    trying this too
    does it still work?

--- createdocs.sh
+++ createdocs.sh
@@ -1,1 +1,18 @@
-python -m sphinx -T -b html -d _build/doctrees -D language=en docs build/html
+#!/bin/bash
+set -e
+
+# === Configuration ===
+MODULE="sphinx"
+TRACE="-T"
+BUILDER="-b html"
+DOCTREES_DIR="-d _build/doctrees"
+EXTRA_OPTS="-D language=en"
+SOURCE_DIR="docs"
+OUTPUT_DIR="build/html"
+
+# === Build command ===
+python -m "$MODULE" $TRACE $BUILDER $DOCTREES_DIR $EXTRA_OPTS "$SOURCE_DIR" "$OUTPUT_DIR"
+
+
+# orig
+#python -m sphinx -T -b html -d _build/doctrees -D language=en docs build/html
